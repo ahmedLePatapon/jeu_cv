5734e3b4cd6d9cfec26e788a5727f7077adbf375
benlebna ahmed <abenlebna@qualycloud.com>
2021-02-15 09:24:46 +0100

    change port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./server');
-const port = process.env.port || 5000 ;
+const port = process.env.PORT || 5000 ;
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
