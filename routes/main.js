8302d0456eaece41914faf31ccbd96fd33ac139a
benlebna ahmed <abenlebna@qualycloud.com>
2021-02-15 13:53:42 +0100

    modification link

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,18 @@
-const {Router} = require('express');
+const { Router } = require('express');
 const fs = require('fs');
 const router = Router();
 
 router.get('/', (req, res) => {
     fs.readFile('./views/index.html', 'utf8', (err, text) => {
+        res.send(text);
+    });
+});
+router.get('/index', (req, res) => res.redirect('/'));
+router.get('/cv', (req, res) => {
+    fs.readFile('./views/cv.html', 'utf8', (err, text) => {
+        console.log('*****************');
+        console.log('okoko', text);
+        console.log('*****************');
         res.send(text);
     });
 });
