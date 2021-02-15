9bc64ce20a5b2d0f21637bc23b1f1dc4381914bf
benlebna ahmed <abenlebna@qualycloud.com>
2021-02-15 15:24:20 +0100

    add favicon

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 const helmet = require('helmet');
+const favicon = require('serve-favicon');
 const BaseRouter = require('./routes');
+
 const app = express();
 
 app.use(express.json());
@@ -17,7 +19,7 @@ app.use('/jquery', express.static(path.join(__dirname, '/node_modules/jquery/dis
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
+app.use(favicon(path.join(__dirname, './src/assets/images', 'favicon.ico')));
 // Security
 if (process.env.NODE_ENV === 'production') {
   app.use(helmet());
